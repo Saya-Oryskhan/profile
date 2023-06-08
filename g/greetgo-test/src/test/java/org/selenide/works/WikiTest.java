814e587698c1f205093685fe7b935bd71c973980
Пользователь <saya.oryskhan@gmail.com>
2023-06-09 01:37:19 +0600

    ^-^

--- g/greetgo-test/src/test/java/org/selenide/works/WikiTest.java
+++ g/greetgo-test/src/test/java/org/selenide/works/WikiTest.java
@@ -9,20 +9,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.*;
+import static com.codeborne.selenide.WebDriverRunner.*;  //использовано, чтобы тесты могли продолжить работу со ссылкой первого теста 
 
 public class WikiTest {
 
     @Test
     public void WikiSearch() {
         open("https://www.wikipedia.org");
-        $("#searchInput").setValue("Зигмунд Фрейд").pressEnter();
+        $("#searchInput").setValue("Зигмунд Фрейд").pressEnter();  //находит поле поиска по айди и вводит запрос
     }
 
 
     @Test
     public void ZaglStr() {
-        if ($(byText("Заглавная страница")).exists()) {
+        if ($(byText("Заглавная страница")).exists()) {            //проверяется существование ссылки для перехода в заглавную страницу
             System.out.println("Ссылка для перехода в заглавную страницу присутствует");
         } else {
             System.out.println("Ссылка для перехода в заглавную страницу отсутствует");
@@ -30,7 +30,7 @@ public class WikiTest {
     }
 
     @Test
-    public void ParagraphCheck() {
+    public void ParagraphCheck() {                               //проверка на наличие хотя бы одного параграфа/тэга р
         ElementsCollection articleParagraphs = $$(By.tagName("p")).shouldHave(CollectionCondition.sizeGreaterThan(0));
         if (articleParagraphs.size() > 0) {
             System.out.println("Состоит из параграфов");
@@ -40,7 +40,7 @@ public class WikiTest {
     }
 
     @Test
-    public void Soderzhanye() {
+    public void Soderzhanye() {                                 //проверка на существование содержания 
         SelenideElement el = $x("//*[text()='Содержание']");
         if (el.exists()) {
             System.out.println("Статья содержит содержание");
@@ -50,11 +50,11 @@ public class WikiTest {
     }
 
     @Test
-    public void TranslateToEn() {
-        $(By.linkText("English")).click();
-        $(By.tagName("body")).shouldBe(visible);
-        String url = getWebDriver().getCurrentUrl();
-        if (url.contains("en")) {
+    public void TranslateToEn() {                   
+        $(By.linkText("English")).click();                      //нажатие на ссылку дял перевода страницы на английский язык
+        $(By.tagName("body")).shouldBe(visible);                //ждем пока страница заугрузится/переведется
+        String url = getWebDriver().getCurrentUrl();            //получить настоящий адрес 
+        if (url.contains("en")) {                               //проверка по адресу ссылки на перевод
             System.out.println("Язык успешно изменен");
         } else {
             System.out.println("Не удалось изменить язык");
