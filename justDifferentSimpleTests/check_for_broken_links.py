1d4c3a78665413af73342e83efa1ae3df2550c2c
Пользователь <saya.oryskhan@gmail.com>
2023-06-20 09:49:33 +0600

    using http request

---
+++ justDifferentSimpleTests/check_for_broken_links.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+import time
+
+driver = webdriver.Chrome()
+driver.set_window_size(1200, 700)
+driver.get("https://www.google.com")
+
+wait = WebDriverWait(driver, 300)
+wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+inputField = wait.until(EC.presence_of_element_located((By.NAME, "q")))
+inputField.send_keys("headless horseman")
+inputField.submit()
+
+wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+links = driver.find_elements(By.TAG_NAME, "a")
+
+for link in links:
+    url = link.get_attribute("href")
+    if url is not None:
+        try:
+            response = requests.head(url)
+            status = response.status_code
+            if status != 200:
+                print(f"Ссылка {url} повреждена. Статус: {status}")
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка при обращении к ссылке {url}: {str(e)}")
+    else:
+        print("Неверный URL: None")
+
+time.sleep(7)
+driver.quit()
